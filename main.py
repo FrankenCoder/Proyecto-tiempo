9b64d31bd434c91b5da7409ce4c6aebed527e2b7
Guillermo A <6uillermo.aguirre@gmail.com>
2021-11-25 13:27:55 -0300

    Se modifico main.py
    Se modifico mensaje de salida del programa.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from datetime import datetime
 print("Programa tiempo")
 tiempo = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print(tiempo)
-print("Saliendo del programa")
+print("Saliendo del programa...Adios!")
