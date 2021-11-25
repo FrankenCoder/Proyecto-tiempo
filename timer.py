05c05c2137a3102b84fe39a4c0aa7e7027d8a7c3
FrankenCoder <6uillermo.aguirre@gmail.com>
2021-11-25 19:25:13 -0300

    se crea archivo timer.py en rama timer

---
+++ timer.py
@@ -0,0 +1,9 @@
+# Rama Timer
+# se crea branch Timer para adicion de funcion timer
+
+from datetime import datetime
+
+print("Programa tiempo")
+tiempo = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print(tiempo)
+print("Saliendo del programa...nos vemos la proxima!")
